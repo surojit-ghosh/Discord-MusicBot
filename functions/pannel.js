f599fbbff9fe1b889a60b457c8b74b025fc04c16
iamsurojit <iamsurojit@users.noreply.github.com>
2022-02-22 23:45:13 +0530

    bach gaya

--- functions/pannel.js
+++ functions/pannel.js
@@ -1,8 +1,8 @@
 import { MessageEmbed, MessageAttachment } from "discord.js";
 
-const pannel = () => {
+const pannel = (client) => {
     const content = '__**Queue list:**__';
-
+    console.log(client.manager)
     const attachment = new MessageAttachment('./structure/assets/music.gif', 'music.gif');
     const embed = new MessageEmbed()
         .setDescription(`Music`)
