62d34c474931ce6d3f5a4ab3fc9b5c6e5a03fda7
iamsurojit <iamsurojit@users.noreply.github.com>
2022-04-25 16:02:40 +0530

    added stats command

---
+++ commands/info/botinfo.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require('discord.js');
+const ms = require('pretty-ms');
+const os = require('node-os-utils');
+
+module.exports = {
+    name: 'botinfo',
+    category: 'info',
+    usage: 'botinfo',
+    cooldown: 5 * 1000,
+    permissions: {
+        client: [],
+        author: []
+    },
+    aliases: [],
+    description: 'Gets info about me',
+    run: async (client, message, args) => {
+        const cpu = await os.cpu.usage();
+        const ram = await os.mem.info();
+        const uptime = ms(client.uptime);
+
+        let embed = new MessageEmbed()
+            .setColor(client.color.default)
+            .setTitle('Information about ' + client.user.username)
+            .setThumbnail(client.user.displayAvatarURL())
+            .setDescription('```\nCPU Usage  :: ' + cpu + ' %' +
+                '\nRam Usage  :: ' + Math.ceil(ram.usedMemMb) + '/' + Math.ceil(ram.totalMemMb) + ' MB' +
+                '\nUptime     :: ' + uptime +
+                '\nPing       :: ' + client.ws.ping + ' ms\n```')
+            .setTimestamp();
+
+        return message.channel.send({ embeds: [embed] });
+    }
+};
